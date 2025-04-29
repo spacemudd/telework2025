d4c9989e6c4d24c4c53b363af83e8c3bb2910481
shafiqalshaar <shafiqalshaar@gmail.com>
2025-04-29 15:37:32 +0300

    Add IBM Plex Arabic font

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['"IBM Plex Arabic"', 'Figtree', ...defaultTheme.fontFamily.sans],
             },
         },
     },
