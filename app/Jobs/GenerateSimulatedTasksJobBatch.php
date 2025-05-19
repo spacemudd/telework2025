664edee8ae3f7d264582847a4b2ed474cefa389c
shafiqalshaar <shafiqalshaar@gmail.com>
2025-05-19 18:27:56 +0300

    Fix parsing tasks

--- app/Jobs/GenerateSimulatedTasksJobBatch.php
+++ app/Jobs/GenerateSimulatedTasksJobBatch.php
@@ -41,7 +41,9 @@ class GenerateSimulatedTasksJobBatch implements ShouldQueue
 
         $data = json_decode($response->json('choices.0.message.content'), true);
 
-        if (!is_array($data)) {
+        if (isset($data['title'], $data['description'])) {
+            $data = [$data];
+        } elseif (!is_array($data)) {
             Log::error('Failed to decode OpenAI response for employee ID ' . $this->employee->id, [
                 'raw_response' => $response->json('choices.0.message.content'),
             ]);
