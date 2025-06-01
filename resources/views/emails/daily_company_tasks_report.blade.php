afc1a959491cb494c4009f5ef8ea21f77eff025c
shafiqalshaar <shafiqalshaar@gmail.com>
2025-06-01 17:49:56 +0300

    Update email look

--- resources/views/emails/daily_company_tasks_report.blade.php
+++ resources/views/emails/daily_company_tasks_report.blade.php
@@ -12,61 +12,25 @@
             direction: rtl;
             font-size: 14px;
         }
-        .header {
-            margin-bottom: 30px;
-        }
-        .company-name {
-            font-size: 16px;
-            font-weight: normal;
-            margin: 0 0 5px 0;
-            text-align: right;
-        }
-        .date {
-            color: #666666;
-            margin: 0 0 20px 0;
-        }
-        .stats {
-            margin: 25px 0;
-            padding: 0;
-            list-style: none;
-        }
-        .stats li {
-            margin-bottom: 10px;
-            text-align: right;
-        }
-        .footer {
-            margin-top: 30px;
-            padding-top: 20px;
-            border-top: 1px solid #eee;
-            color: #666666;
-            font-size: 13px;
-            text-align: right;
-        }
     </style>
 </head>
 <body>
+    <p>السادة الكرام /  {{$company->name}}</p>
+    <p>مرفق لكم تقرير النشاط ليوم -  {{ \Carbon\Carbon::parse($yesterday)->translatedFormat('l j F Y') }}</p>
+    <p>---------------------------------</p>
+    <ul class="stats">
+        <li>مهام جديدة: {{ $statistics['new'] }}</li>
+        <li>مهام محدثة: {{ $statistics['updated'] }}</li>
+        <li>مهام مكتملة: {{ $statistics['completed'] }}</li>
+    </ul>
+    <p>---------------------------------</p>
     <div>
-        <div class="header">
-            <h1 class="company-name">{{ $company->name }}</h1>
-            <p class="date">{{ \Carbon\Carbon::parse($yesterday)->translatedFormat('l j F Y') }}</p>
-        </div>
-
-        <div>تقرير المهام اليومي</div>
-
-        <ul class="stats">
-            <li>مهام جديدة: {{ $statistics['new'] }}</li>
-            <li>مهام محدثة: {{ $statistics['updated'] }}</li>
-            <li>مهام مكتملة: {{ $statistics['completed'] }}</li>
-        </ul>
-
-        <div>
-            تم إرفاق ملف يحتوي على تفاصيل جميع المهام بصيغة CSV.
-        </div>
-
-        <div class="footer">
-            هدف،<br>
-            تحياتنا
-        </div>
+        تم إرفاق ملف يحتوي على تفاصيل جميع المهام بصيغة CSV.
+    </div>
+    <p>---------------------------------</p>
+    <div class="footer" style="margin-top: 20px; font-size: 12px; color: #777;">
+        هدف،<br>
+        تحياتنا
     </div>
 </body>
 </html>
