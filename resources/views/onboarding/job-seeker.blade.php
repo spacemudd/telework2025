7ac25e3ce7aaf6067b6c722b4c37b1597129afe9
shafiqalshaar <shafiqalshaar@gmail.com>
2025-08-27 15:07:37 +0300

    Use old name

--- resources/views/onboarding/job-seeker.blade.php
+++ resources/views/onboarding/job-seeker.blade.php
@@ -25,7 +25,7 @@
                                    type="text" 
                                    autocomplete="name" 
                                    required 
-                                   value="{{ old('full_name') }}"
+                                   value="{{ old('full_name', auth()->user()->name) }}"
                                    class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm">
                         </div>
                         @error('full_name')
